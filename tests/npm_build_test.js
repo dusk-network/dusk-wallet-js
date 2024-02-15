2ce4884154009ebbc87c4768e80373277f999c5e
Daksh <work@dakshu.xyz>
2024-02-15 04:49:04 -0500

    Deno info

--- tests/npm_build_test.js
+++ tests/npm_build_test.js
@@ -45,8 +45,8 @@ Deno.test({
 Deno.test({
   name: "console.log hash of the dusk_wallet_core.js",
   async fn() {
-    const command = new Deno.Command("diff", {
-      args: ["./assets/wallet.js", "./dist/wallet.js"],
+    const command = new Deno.Command("deno", {
+      args: ["info", "./src/mod.js"],
     });
 
     // create subprocess and collect output
